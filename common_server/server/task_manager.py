3bd7841f5ebf35312de30ce0af59c6e662ea8447
pavanmantha <pavan.mantha@thevaslabs.io>
2025-04-27 21:44:10 +0530

    changed module name for better serperation

--- common_server/server/task_manager.py
+++ common_server/server/task_manager.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Union, AsyncIterable, List
-from common.types import Task
-from common.types import (
+from common_server.types import Task
+from common_server.types import (
     JSONRPCResponse,
     TaskIdParams,
     TaskQueryParams,
@@ -31,7 +31,7 @@ from common.types import (
     TaskPushNotificationConfig,
     InternalError,
 )
-from common.server.utils import new_not_implemented_error
+from common_server.server.utils import new_not_implemented_error
 import asyncio
 import logging
 
